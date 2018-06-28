b363b4165fc754807afbba24f18bb195141a1537
Lucas <lucas.mironne@gmail.com>
2018-06-28 19:05:31 +0200

    Ajout du générateur d'arène

---
+++ php/query/arena.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LMironne
+ * Date: 28/06/2018
+ * Time: 18:47
+ */
+
+include("../tools/api_conf.php");
+include("../tools/database.php");
+
+$apiResult = file_get_contents("https://api.royaleapi.com/constants", true, $context);
+$data = json_decode($apiResult, true);
+
+$insertPattern = "
+INSERT INTO arena
+VALUES('', %d, \"%s\", \"%s\", %d)
+";
+
+foreach ($data['arenas'] as $arena) {
+    $insertQuery = utf8_decode(sprintf(
+        $insertPattern, $arena['arena_id'], $arena['subtitle'], $arena['title'], $arena['trophy_limit']
+    ));
+    execute_query($db, $insertQuery);
+}
