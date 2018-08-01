3dd235449adfbcfc23633b4ac9893ca6d854acee
BadPoussin <boudiwam@gmail.com>
2018-08-01 18:38:53 +0200

    WIP - Page de recherche de joueur

--- rules.php
+++ rules.php
@@ -18,28 +18,28 @@
 <?php include("header.html"); ?>
 <div class="container">
     <h1 class="whiteShadow">Réglement</h1>
-    <br/>
+    <br>
     <hr/>
     <h3 class="whiteShadow">Promotions</h3>
     <p class="whiteShadow">Pour devenir Ainé, il faut participer à au moins <b>3 guerres</b></p>
-    <br/>
+    <br>
     <p class="whiteShadow">Pour devenir Chef Adjoint, il faut avoir recruter au moins <b>5 joueurs qui sont devenus Ainés</b></p>
-    <br/>
+    <br>
     <hr/>
     <h3 class="whiteShadow">Rétrogradations et Exlusions</h3>
-    <br/>
-    <p class="whiteShadow">Les conditions de rétrogradation dépendent de la participation du joueur à la guerre.<br/><br/>Voici les conditions :</p><br/>
+    <br>
+    <p class="whiteShadow">Les conditions de rétrogradation dépendent de la participation du joueur à la guerre.<br><br>Voici les conditions :</p><br>
     <li class="list-unstyled">
         <ul class="whiteShadow">- Il ne participe pas à 10 guerres consécutives</ul>
         <ul class="whiteShadow">- Il participe que partiellement aux collections (5 matchs injoués)</ul>
         <ul class="whiteShadow">- Il ne participe pas à 2 matchs de guerre dans la saison</ul>
         <ul class="whiteShadow">- Il ne participe pas à 4 matchs de guerre au total</ul>
     </li>
-    <br/>
-    <p class="whiteShadow">Si un joueur Ainé remplit l'une de ces conditions, il sera rétrogradé au rang de Membre. <br/>Si un membre remplit l'une de ces conditions, il sera exclu du clan</p>
-    <br/>
+    <br>
+    <p class="whiteShadow">Si un joueur Ainé remplit l'une de ces conditions, il sera rétrogradé au rang de Membre. <br>Si un membre remplit l'une de ces conditions, il sera exclu du clan</p>
+    <br>
     <hr/>
-    <br/>
+    <br>
 </div>
 <?php include("footer.html"); ?>
 </body>
---
+++ search.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADufresne
+ * Date: 01/08/2018
+ * Time: 17:13
+ */
+
+include(__DIR__ . "/tools/database.php");
+$lastUpdated = getLastUpdated($db, "index");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Iron</title>
+    <?php include("head.php"); ?>
+    <script>
+        $(document).ready(function () {
+            $('#tableIndex').on('click', 'tbody td', function () {
+                $("body").css("cursor", "wait");
+                window.location = $(this).closest('tr').find('.linkToPlayer').attr('href');
+            });
+
+            $('#tx_search').on("keyup paste", function () {
+                let value = $(this).val().toLowerCase();
+                const playerLine = $('.playerTr');
+                if (value.length === 0) {
+                    playerLine.show();
+                    return;
+                }
+
+                playerLine.each(function () {
+                    if ($(this).next().val().toLowerCase().indexOf(value) < 0)
+                        $(this).hide();
+                    else
+                        $(this).show();
+                });
+            });
+
+            $("#valueSearched").keydown(function (e) {
+                if (e.keyCode === 32) {
+                    return false;
+                }
+            });
+        });
+
+        function launchSearch() {
+            var valueSearched = document.getElementById('valueSearched').value;
+
+            if (valueSearched !== "") {
+                if (valueSearched.charAt(0) === '#') {
+                    valueSearched = valueSearched.substring(1);
+                }
+
+                var date = new Date();
+                date.setTime(+ date + (365 * 86400000));
+                document.cookie = "playerTag=" + valueSearched + ";expires=" + date.toUTCString();
+
+                window.location.replace("player.php?tag=" + valueSearched);
+            }
+        }
+    </script>
+</head>
+<body>
+<?php include("header.html"); ?>
+<div class="container">
+    <h1 class="whiteShadow">Recherche de joueur</h1><br>
+    <div>
+        <span class="whiteShadow">Entrez le tag, le nom ou le grade du joueur pour accéder à la page personnalisée correspondante.</span><br>
+        <input type="text" placeholder="Tag" id="valueSearched">
+        <button name="btLaunchSearch" onclick="launchSearch()">Valider</button><br><br>
+        <span class="whiteShadow"><a href="index.php">Ou cliquez ici pour accéder au clan.<a></a></span>
+    </div>
+
+    <br>
+</div>
+<div id="loaderDiv">
+    <img id="loaderImg" src="images/loader.gif"/>
+</div>
+<div class="row text-center">
+    <?php if ($lastUpdated['updated'] != null):
+        $time = strtotime($lastUpdated['updated']);
+        ?>
+        <span class="whiteShadow">Dernière mise à jour le : <b><?php echo '' . date('d/m/Y', $time) ?></b> à <b><?php echo '' . date('H:i', $time) ?></span>
+    <?php else: ?>
+        <span class="whiteShadow">Nécessite une mise à jour</span>
+    <?php endif; ?>
+</div>
+<?php include("footer.html"); ?>
+</body>
+</html>
