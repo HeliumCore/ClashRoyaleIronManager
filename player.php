5212a7fe6ad19662cb8876a94ba58c34e145e48a
availlant <contact@arnaudvaillant.fr>
2018-08-22 23:22:08 +0200

    Refonte de la page player

--- player.php
+++ player.php
@@ -11,11 +11,15 @@ include(__DIR__ . "/tools/api_conf.php");
 include_once(__DIR__ . "/check_login.php");
 
 $playerTag = explode("/", substr($_SERVER['REQUEST_URI'], 1))[1];
+
+if(isset($_GET['tag'])) $playerTag = $_GET['tag'];
+
 if (empty($playerTag)) {
     header('Location: /clan');
 }
 
 $player = getPlayerInfos($db, $playerTag);
+
 $crIds = explode(",", $player['cr_ids']);
 $deckLinkPattern = "https://link.clashroyale.com/deck/fr?deck=%d;%d;%d;%d;%d;%d;%d;%d";
 $deckLink = sprintf(
@@ -59,38 +63,53 @@ $lastUpdated = getLastUpdatedPlayer($db, $playerTag);
 <body>
 <?php include("header.php"); ?>
 <div class="container">
-    <!--    TODO refaire le design de la page (ci dessous, le bandeau du nom du joueur/trophées) -->
-    <!--    <div class="row">-->
-    <!--        <div class="col-md-5 player-badge">-->
-    <!--            <h3 class="pull-left whiteShadow"><img src="images/coin.png" height="40px"/>&nbsp;-->
-    <?php //print $player['playerName']; ?><!--</h3>-->
-    <!--            <div class="pull-right whiteShadow trophy-div">-->
-    <!--                <img src="images/ui/trophy.png" height="30px" class="trophy-img"/>&nbsp;--><?php //print $player['trophies']; ?>
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--    </div>-->
+    <div class="player-badge">
+        <div class="pull-right whiteShadow trophy-div">
+            <img src="/images/ui/trophy.png" height="30px" class="trophy-img"/>
+                <span><?php print $player['trophies']; ?></span>
+                <div class="max-trophy">Max <?php print $player['max_trophies'] ?></div>
+        </div>
+        <div class="flex"> 
+            <div class="level-container">
+                <span><?php print $player['level'] ?></span>
+                <img src="/images/ui/level-big.png" class="player-level"/>
+            </div> 
+            <h1 class="whiteShadow">
+                <?php print utf8_encode($player['playerName']); ?>
+                <br>
+                <span class="small"><?php print utf8_encode($player['playerRole']) ?></span>&nbsp;<span class="tiny"><?php print utf8_encode($player['tag']); ?></span>
+            </h1>
+        </div>
+        <div>
+            Rang: <?php print $player['rank']; ?> <br>
+            <?php print $player['arena']; ?>
+        </div>
+    </div>
+</div>
+<div class="container">
     <div class="row">
         <div class="col-md-5">
             <h3 class="whiteShadow">Coffres à venir</h3>
             <div class="row text-center" id="chestsDiv">
                 <?php for ($i = 0; $i <= 7; $i++): ?>
-                    <div class="col-xs-3">
-                        <div class="img-responsive chests-placeholder">
-                            <img src="/images/chests/legendary-chest.png" alt="failed to load img"
-                                 class="img-responsive chests"/>
-                            <span class="chestNumber whiteShadow">+</span>
-                        </div>
+                <div class="col-xs-3">
+                    <div class="img-responsive chests-placeholder">
+                        <img src="/images/chests/legendary-chest.png" alt="failed to load img" class="img-responsive chests"/>
+                        <span class="chestNumber whiteShadow">+</span>
                     </div>
+                </div>
                 <?php endfor; ?>
             </div>
         </div>
         <div class="col-md-5 col-md-offset-2">
+            <div class="wrapper-current-deck">
             <h3 class="whiteShadow">Deck du moment</h3>
-            <div class="row">
+                
                 <?php
                 $cardKeys = explode(",", $player['card_keys']);
-                if (sizeof($cardKeys) > 0):
-                    for ($i = 0; $i <= 7; $i++):?>
+                if (sizeof($cardKeys) > 0): ?>
+                    <div class="row">
+                    <?php for ($i = 0; $i <= 7; $i++):?>
                         <div class="col-xs-3">
                             <div class="img-responsive">
                                 <img src="/images/cards/<?php print $cardKeys[$i]; ?>.png"
@@ -98,44 +117,35 @@ $lastUpdated = getLastUpdatedPlayer($db, $playerTag);
                             </div>
                         </div>
                     <?php endfor; ?>
-                    <div id="deckLinkDiv" class="text-center pointerHand">
-                        <a href="<?php print $deckLink; ?>" class="text-center">
-                            <img src="/images/ui/copy-deck.png" class="deckLink" alt="Copier le lien">
-                            <span id="spanDeckLink" class="whiteShadow text-center">Copier le deck</span>
-                        </a>
-                    </div>
+                </div>
+                <div id="deckLinkDiv" class="text-center pointerHand">
+                    <a href="<?php print $deckLink; ?>" class="text-center">
+                        <img src="/images/ui/copy-deck.png" class="deckLink" alt="Copier le lien">
+                        <span id="spanDeckLink" class="whiteShadow text-center">Copier</span>
+                    </a>
+                </div>
+                <div class="elixir-average">
+                    Cout moyen en élixir: 2.9
+                </div>
+            </div>
                 <?php else: ?>
                     <div>
                         <span class="whiteShadow text-center">Actualisez les informations pour voir votre deck</span>
                     </div>
                 <?php endif; ?>
-            </div>
         </div>
     </div>
-    <br><br>
-    <h3 class="whiteShadow">Joueur</h3>
+    <h3 class="whiteShadow">Dons</h3>
     <div class="table-responsive">
+
         <table class="table">
             <thead>
             <tr class="rowIndex">
-                <th class="headIndex">Rang</th>
-                <th class="headIndex text-center table-border" colspan="3">Joueur</th>
-                <th class="headIndex text-center table-border" colspan="2">Trophées</th>
-                <th class="headIndex text-center">Arène</th>
-                <th class="headIndex text-center table-border-left" colspan="2">Dons</th>
             </tr>
             </thead>
             <tbody>
             <?php
             echo '<tr>';
-            echo '<input id="playerTagHidden" type="hidden" name="playerTagHidden" value="' . $player['tag'] . '" />';
-            echo '<td class="whiteShadow">' . $player['rank'] . '</td>';
-            echo '<td id="playerTag" class="whiteShadow text-center table-border-left">' . $player['tag'] . '</td>';
-            echo '<td class="whiteShadow text-center">' . utf8_encode($player['playerName']) . '<br>' . utf8_encode($player['playerRole']) . '</td>';
-            echo '<td class="whiteShadow text-center">Niveau<br>' . $player['level'] . '</td>';
-            echo '<td class="whiteShadow text-center table-border-left">Courant<br>' . $player['trophies'] . '</td>';
-            echo '<td class="whiteShadow text-center">Max<br>' . $player['max_trophies'] . '</td>';
-            echo '<td class="whiteShadow text-center table-border">' . $player['arena'] . '</td>';
             echo '<td class="whiteShadow text-center">Données<br>' . $player['donations'] . '</td>';
             echo '<td class="whiteShadow text-center">Reçues<br>' . $player['received'] . '</td>';
             echo '</tr>';
@@ -143,27 +153,33 @@ $lastUpdated = getLastUpdatedPlayer($db, $playerTag);
             </tbody>
         </table>
     </div>
-    <h3 class="whiteShadow">Guerres</h3>
+    <h3 class="whiteShadow">Guerres <span class="small whiteShadow">(&nbsp;<?php print $player['total_war_played'] ?> jouées&nbsp;)</span></h3>
     <div class="table-responsive">
         <table class="table">
             <thead>
             <tr class="rowIndex">
-                <th class="headIndex">Guerres</th>
-                <th class="headIndex text-center table-border" colspan="3">Collections</th>
-                <th class="headIndex text-center" colspan="2">Batailles</th>
+                <th class="headIndex text-center table-border" colspan="6">Collections</th>
             </tr>
             </thead>
             <tbody>
-            <?php
-            echo '<tr>';
-            echo '<td class="whiteShadow">Jouées<br>' . $player['total_war_played'] . '</td>';
-            echo '<td class="whiteShadow text-center table-border-left">Jouées<br>' . $player['total_collection_played'] . '</td>';
-            echo '<td class="whiteShadow text-center">Gagnées<br>' . $player['total_collection_won'] . '</td>';
-            echo '<td class="whiteShadow text-center"><img src="/images/ui/deck.png" height="35px"/>&nbsp;' . $player['total_cards_earned'] . '</td>';
-            echo '<td class="whiteShadow text-center table-border-left">Jouées<br>' . $player['total_battle_played'] . '</td>';
-            echo '<td class="whiteShadow text-center">Gagnées<br>' . $player['total_battle_won'] . '</td>';
-            echo '</tr>';
-            ?>
+                <tr>
+                    
+            
+                    <td class="whiteShadow text-center table-border-left" colspan="2">Jouées<br><?php print $player['total_collection_played'] ?></td>
+                    <td class="whiteShadow text-center" colspan="2">Gagnées<br><?php print $player['total_collection_won'] ?></td>
+                    <td class="whiteShadow text-center" colspan="2"><img src="/images/ui/deck.png" height="35px"/><?php print $player['total_cards_earned'] ?></td>
+                </tr>
+            </tbody>
+            <thead>
+            <tr class="rowIndex">
+                <th class="headIndex text-center" colspan="6">Batailles</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="whiteShadow text-center table-border-left" colspan="3">Jouées<br><?php print $player['total_battle_played'] ?></td>
+                    <td class="whiteShadow text-center" colspan="3">Gagnées<br><?php print $player['total_battle_won'] ?></td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -186,11 +202,11 @@ $lastUpdated = getLastUpdatedPlayer($db, $playerTag);
 <div id="loaderDiv">
     <img id="loaderImg" src="/images/loader.gif"/>
 </div>
-<div class="row text-center">
+<div class="text-center">
     <?php if ($lastUpdated['updated'] != null):
         $time = strtotime($lastUpdated['updated']);
         ?>
-        <span class="whiteShadow">Dernière mise à jour le : <b><?php echo '' . date('d/m/Y', $time) ?></b> à <b><?php echo '' . date('H:i', $time) ?></span>
+        <span class="whiteShadow">Dernière mise à jour le : <b><?php print date('d/m/Y', $time); ?></b> à <b><?php print date('H:i', $time); ?></span>
     <?php else: ?>
         <span class="whiteShadow">Nécessite une mise à jour</span>
     <?php endif; ?>
